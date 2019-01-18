ab0221878e021bbc14586e8fbfdb5e7405277076
Rachele <rachele.avella@gmail.com>
2019-01-18 13:44:05 -0800

    fixed mailchimp redirect

--- Collabathon/main.js
+++ Collabathon/main.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.join-our-newsletter-2').hide();
@@ -13,5 +9,37 @@ $(document).ready(function(){
     $('.cross').on('click', function () {
         $('.join-our-newsletter-2').hide();
     })
+
+    
 });
+
+$("#subscribe-form").submit(function(e){
+    e.preventDefault();
+    submitSubscribeForm($("#subscribe-form"));
+
+    function submitSubscribeForm($form, $resultElement) {
+        $.ajax({
+            type: "GET",
+            url: $form.attr("action"),
+            data: $form.serialize(),
+            cache: false,
+            dataType: "jsonp",
+            jsonp: "c",
+            contentType: "application/json; charset=utf-8",
+    
+            error: function(error){},
+    
+            success: function(data){
+                if (data.result == "success") {
+                    $('.join-our-newsletter-2').hide()
+                    $('.join-our-newsletter-3').show()
+                    $('.cross').on('click', function () {
+                        $('.join-our-newsletter-3').hide();
+                    })
+                }
+            }
+        }
+    )};
+});
+
 
