873f3b8e07ae5716a556ba088747e11f50e964ab
harrystacey123 <harrystacey@live.co.uk>
2019-01-18 12:11:20 -0800

    added form

--- Collabathon/main.js
+++ Collabathon/main.js
@@ -1,4 +1,17 @@
+
+
+
+
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.join-our-newsletter-2').hide();
+    $('.join-our-newsletter').show();
+    $('.join-our-newsletter').on('click', function() {
+        $('.join-our-newsletter-2').show();
+    })
+    $('.cross').on('click', function () {
+        $('.join-our-newsletter-2').hide();
+    })
 });
 
